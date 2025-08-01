12f4c29638352e9fd38a9596833539f920d9b1cc
riyathakur11 <riya@code4each.com>
2025-08-01 19:39:25 +0530

    ui and functionality chnages for the signin/signup

--- src/components/common/modals/AuthSignupModal.vue
+++ src/components/common/modals/AuthSignupModal.vue
@@ -1,3 +1,6 @@
+<style>
+@import "@/assets/signup.css";
+</style>
 <template>
   <div v-if="showSignUpModal" class="modal-backdrop fade show"></div>
   <div
@@ -21,81 +24,91 @@
             >
               <i class="fa fa-times"></i>
             </button>
+            <a class="navbar-brand-logo" style="cursor: pointer;text-align: center;">
+              <img class="imgisite" src="/images/logo-beta.png" alt="logo" style="width: 40%;">
+            </a>
+            <h4 class="sevenDays">Start Your 7 Day Free Trial Today!</h4>
 
-            <h1>Start Your 7 Day Free Trial Today!</h1>
+            <div class="form-start">
+              <div class="main-form1" style="display: flex;justify-content: space-evenly;flex-wrap: wrap;">
+                <!-- <div class="main-form-signup"> -->
+                  <!-- <div class="form-group"> -->
+                    <!-- <label for="exampleInputName">Company Name</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="exampleInputName"
+                      placeholder="Company  Name"
+                      v-model="formData.company_name"
+                    />
+                    <div class="text-danger">{{ allErrors.company_name }}</div> -->
+                  <!-- </div> -->
 
-            <form class="form-start">
-              <div class="main-form">
-                <div class="form-group">
-                  <label for="exampleInputName">Company Name</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="exampleInputName"
-                    placeholder="Company  Name"
-                    v-model="formData.company_name"
-                  />
-                  <div class="text-danger">{{ allErrors.company_name }}</div>
+                  <div class="form-group signup">
+                    <label for="exampleInputName">Name</label>
+                    <input
+                      type="name"
+                      class="form-control"
+                      id="exampleInputName"
+                      placeholder="Name"
+                      v-model="formData.name"
+                    />
+                    <div class="text-danger">{{ allErrors.name }}</div>
+                  </div>
+                <!-- </div> -->
+                <!-- <div class="main-form-signup"> -->
+                  <div class="form-group signup">
+                    <label for="exampleInputEmail1">Email </label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Email"
+                      v-model="formData.email"
+                    />
+                    <div class="text-danger">{{ allErrors.email }}</div>
+                  <!-- </div> -->
+                  <!-- <div class="form-group">
+                    <label for="exampleInputPassword1">Phone no.</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="exampleInputPassword1"
+                      placeholder="Phone Number..."
+                      v-model="formData.phone"
+                    />
+                    <div class="text-danger">{{ allErrors.phone }}</div>
+                  </div> -->
                 </div>
-
-                <div class="form-group">
-                  <label for="exampleInputName">Name</label>
-                  <input
-                    type="name"
-                    class="form-control"
-                    id="exampleInputName"
-                    placeholder="Name"
-                    v-model="formData.name"
-                  />
-                  <div class="text-danger">{{ allErrors.name }}</div>
-                </div>
+                <!-- <div class="main-form-signup"> -->
+                  <div class="form-group signup" style="position: relative;">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input
+                      :type="showPassword ? 'text' : 'password'"
+                      class="form-control"
+                      id="exampleInputPassword1"
+                      placeholder="Password"
+                      v-model="formData.password"
+                    />
+                     <!-- Toggle icon -->
+                    <i
+                      :class="showPassword ? 'fa fa-eye' : 'fa fa-eye-slash'"
+                      @click="togglePasswordVisibility"
+                      style="position: absolute;top: 58px;right: 15px;cursor: pointer;font-size: 18px;"
+                    ></i>
+                    <div class="text-danger">{{ allErrors.password }}</div>
+                    <div v-if="backendError" class="text-danger">{{ backendError }}</div>
+                  </div>
+                <!-- </div> -->
               </div>
-              <div class="main-form">
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Email </label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Email"
-                    v-model="formData.email"
-                  />
-                  <div class="text-danger">{{ allErrors.email }}</div>
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Phone no.</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="exampleInputPassword1"
-                    placeholder="Phone Number..."
-                    v-model="formData.phone"
-                  />
-                  <div class="text-danger">{{ allErrors.phone }}</div>
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Password"
-                  v-model="formData.password"
-                />
-                <div class="text-danger">{{ allErrors.password }}</div>
-              </div>
-              <div class="text-danger">{{ backendError }}</div>
-              <div class="form-group mt-3">
-              </div>
-
-              <div class="dual-logo">
+              <div class="signup-button" style="text-align: center;">
                 <button
                   type="submit"
                   class="btn btn-primary1"
                   @click="registerUser"
                   :disabled="isDisabledSignUp"
+                  style="width: 71%;margin: 15px auto;"
                 >
                   Sign Up
                 </button>
@@ -105,7 +118,10 @@
                   <div class="three-body__dot1"></div>
                 </div>
               </div>
-            </form>
+              <div class="dontAcc">
+                <a href="javascript:void(0)" @click.prevent="emits('showAnotherModal', 'login')" class="signUp-btn">Already have an account?</a>
+              </div>
+          </div>
           </div>
           <div>
             <svg
@@ -135,15 +151,15 @@
           </div>
           <div class="column" id="secondary">
             <div class="sec-content">
-              <h2>Welcome Back!</h2>
-              <h3>Already have an account?</h3>
-              <button
+              <h2>New Here!</h2>
+              <!-- <h3>Already have an account?</h3> -->
+              <!-- <button
                 type="button"
                 @click="emits('showAnotherModal', 'login')"
                 class="btn btn-primary"
               >
                 Login
-              </button>
+              </button> -->
               <GoogleLogin />
             </div>
           </div>
@@ -215,12 +231,14 @@ const validationSchema = yup.object({
 
 const registerUser = handleSubmit(async () => {
   try {
+    console.log("Clicked Sign Up", formData.value);
     isDisabledSignUp.value = true;
     loadingSignup.value = true;
     await validationSchema.validate(formData.value, { abortEarly: false });
     allErrors.value = {};
 
     const response = await WordpressService.registerUser(formData.value);
+    console.log(response);
     if (response.status === 200 && response.data.success) {
       const token = response.data.token;
       localStorage.setItem("access_token", token);
@@ -257,7 +275,14 @@ const hideSignupModal = () => {
   showSignUpModal.value = false;
   formData.value = {};
   allErrors.value = {};
+  backendError.value = "";
+  showPassword.value = false;  
   emits('closeModal');
+};
+
+const showPassword = ref(false);
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value;
 };
 
 </script>
